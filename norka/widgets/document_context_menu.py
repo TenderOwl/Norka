6ea81e4736559b5c45284f4897d0bdede7cfb842
Andrey Maksimov <meamka@ya.ru>
2020-05-15 10:46:40 +0300

    Rearrange context menu items

--- norka/widgets/document_context_menu.py
+++ norka/widgets/document_context_menu.py
@@ -43,9 +43,9 @@ class DocumentContextMenu(Gtk.Menu):
         delete_menu = Gtk.MenuItem(action_name='document.delete')
         delete_menu.add(Granite.AccelLabel.from_action_name('Delete', 'document.delete'))
 
-        self.append(export_menu)
-        self.append(Gtk.SeparatorMenuItem())
         self.append(rename_menu)
+        self.append(Gtk.SeparatorMenuItem())
+        self.append(export_menu)
         self.append(Gtk.SeparatorMenuItem())
         self.append(archive_menu)
         self.append(delete_menu)
