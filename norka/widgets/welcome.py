995a10a458231822ab34eab7c5200f97a251deb1
Sabri Ünal <libreajans@gmail.com>
2024-01-09 17:04:10 +0300

    app: Fix an error
    Fix gettext call in comment line

--- norka/widgets/welcome.py
+++ norka/widgets/welcome.py
@@ -14,7 +14,6 @@
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
 #
-from gettext import gettext as _
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
